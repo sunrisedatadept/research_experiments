92fccdaee3fe09f1b646207052e3de6ab09f274c
Brittany Bennett <thebrittanybennett@gmail.com>
2021-11-15 09:59:56 -0500

    Bring back VAN class

--- cal_tmc_welcome_series_test/random_sample_process.py
+++ cal_tmc_welcome_series_test/random_sample_process.py
@@ -60,6 +60,9 @@ everyaction_headers = {"headers" : "application/json"}
 # Instatiate Redshift instance
 rs = Redshift()
 
+# Instantiate VAN instance
+van = VAN(api_key=van_key, db=EveryAction)
+
 ##### Set up logger #####
 logger = logging.getLogger(__name__)
 _handler = logging.StreamHandler()
