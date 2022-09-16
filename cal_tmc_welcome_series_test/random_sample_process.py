daff3e87bd6da4d45661d07ed3a1eec71226527b
Vianney Martinez <68610712+vamartinezi@users.noreply.github.com>
2022-09-16 13:54:33 -0500

    Update random_sample_process.py
    added logging to line 123

--- cal_tmc_welcome_series_test/random_sample_process.py
+++ cal_tmc_welcome_series_test/random_sample_process.py
@@ -120,7 +120,8 @@ def get_export_job(everyaction_download_url, everyaction_headers, everyaction_au
     	time.sleep(20) # twenty second delay
     	try:
     		response = requests.get(everyaction_download_url, headers = everyaction_headers, auth = everyaction_auth)
-    		downloadLink = response.json().get('files')[0].get('downloadUrl')
+    		logger.info(response)
+            downloadLink = response.json().get('files')[0].get('downloadUrl')
     		break
     	except:
     		logger.info("File not ready, trying again in 20 seconds")
